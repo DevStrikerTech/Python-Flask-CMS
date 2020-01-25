2fa011dec07001fbd71fcac31c8ba94f74b816fe
KingCobra2018 <kcobra557@gmail.com>
2020-01-25 21:09:50 +0000

    added regex for email validator

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from flask_mysqldb import MySQL
-from wtforms import Form, PasswordField, validators
-from wtforms.fields.html5 import EmailField
+from wtforms import Form, StringField, PasswordField, validators
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
@@ -11,16 +10,19 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-# Register form using WTForms(RegisterForm Class)
+# Register Form using WTForms RegisterForm Class
 class RegisterForm(Form):
-    email = EmailField('Email', [validators.DataRequired(), validators.Email()])
+    email = StringField('Email', [
+        validators.DataRequired(),
+        validators.Regexp('(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', message="Enter valid email address")
+    ])
     password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Password do not match')
     ])
     confirm = PasswordField('Confirm Password')
 
-# Route for Register
+# Route for register.html
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
